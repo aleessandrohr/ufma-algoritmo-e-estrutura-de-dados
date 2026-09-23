cf2cd7d4c921f68227237ae3528fa39e9a70b39d
Alessandro Henrique Ramos <alessandro.henri.r@gmail.com>
2026-09-23 12:00:22 -0300

    format

--- idade.c
+++ idade.c
@@ -1,56 +1,57 @@
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 
 int main(void) {
-	int nascimento, anoAtual, idade, tamanhoPode;
-	bool podeNada, podeVotar, podeDirigir, podeAbrirEmpresa;
-	char *pode[3];
+  int nascimento, anoAtual, idade, tamanhoPode;
+  bool podeNada, podeVotar, podeDirigir, podeAbrirEmpresa;
+  char *pode[3];
 
-	tamanhoPode = 0;
-	anoAtual = 2026;
+  tamanhoPode = 0;
+  anoAtual = 2026;
 
-	printf("Digite o ano atual: ");
-	scanf("%d", &nascimento);
+  printf("Digite o ano atual: ");
+  scanf("%d", &nascimento);
 
-	idade = anoAtual - nascimento;
+  idade = anoAtual - nascimento;
 
-	printf("Voce tem %d anos!\n", idade);
+  printf("Voce tem %d anos!\n", idade);
 
-	podeNada = idade < 16;
+  podeNada = idade < 16;
 
-	if (podeNada) {
-		printf("Voce nao pode votar, dirigir ou abrir uma empresa!\n");
+  if (podeNada) {
+    printf("Voce nao pode votar, dirigir ou abrir uma empresa!\n");
 
-		return 0;
-	}
+    return 0;
+  }
 
-	podeVotar = idade >= 16;
+  podeVotar = idade >= 16;
 
-	if (podeVotar) {
-		pode[0] = "votar";
-		tamanhoPode++;
-	}
+  if (podeVotar) {
+    pode[0] = "votar";
+    tamanhoPode++;
+  }
 
-	podeDirigir = idade >= 18;
+  podeDirigir = idade >= 18;
 
-	if (podeDirigir) {
-		pode[1] = "dirigir";
-		tamanhoPode++;
-	}
+  if (podeDirigir) {
+    pode[1] = "dirigir";
+    tamanhoPode++;
+  }
 
-	podeAbrirEmpresa = idade >= 21;
+  podeAbrirEmpresa = idade >= 21;
 
-	if (podeAbrirEmpresa) {
-		pode[2] = "abrir uma empresa";
-		tamanhoPode++;
-	}
+  if (podeAbrirEmpresa) {
+    pode[2] = "abrir uma empresa";
+    tamanhoPode++;
+  }
 
-	printf("Voce pode fazer as seguintes coisas: ");
+  printf("Voce pode fazer as seguintes coisas: ");
 
-	for (int i = 0; i < tamanhoPode; i++) {
-			if (i == tamanhoPode - 1) {
-				printf("%s.\n", pode[i]);
-			} else
-			printf("%s, ", pode[i]);
-	}
+  for (int i = 0; i < tamanhoPode; i++) {
+    if (i == tamanhoPode - 1) {
+      printf("%s.\n", pode[i]);
+    } else {
+      printf("%s, ", pode[i]);
+    }
+  }
 }
